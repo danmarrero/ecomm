6863af49ea3cedb219586d14d6e0e48230961f67
Daniel Marrero <dan.marrero@gmail.com>
2018-04-27 20:02:37 +0000

    script commit

--- cc.R
+++ cc.R
@@ -1,28 +1,32 @@
 # Contribution Code Calculations
 
+library(bigrquery)
+library(tidyverse)
+
+project <- "ecomm-197702"
+sql <- "SELECT * FROM [ecomm-197702:ecomm.sales_lxw]"
+sales_lxw <- query_exec(sql, project = project)
+
 cc <- sales_lxw %>%
-  select(UPC, PRODUCT_TYPE, L13W_SLS_U)
+  select(upc, l13w_sls) %>%
+  arrange(desc(l13w_sls)) %>%
+  mutate(cum_sum = cumsum(l13w_sls)) %>%
+  mutate(ttl_sum = sum(l13w_sls)) %>%
+  mutate(sum_pct = cum_sum / ttl_sum) %>%
+  mutate(cc = if_else(sum_pct <= 0.60, "A", if_else(
+                      sum_pct <= 0.80, "B", if_else(
+                      sum_pct <= 0.95, "C", if_else(
+                      sum_pct <= 0.99, "D", if_else(
+                      sum_pct <= 1.00, "E", "E"))))))
 
 cc <- cc %>%
-  group_by(PRODUCT_TYPE) %>%
-  mutate(CUM_SUM = cumsum(L13W_SLS_U))
+  select(upc, cc)
 
-cc_group <- cc %>%
-  group_by(PRODUCT_TYPE) %>%
-  summarise(sum(L13W_SLS_U))
-
-names(cc_group)[2] <- "TTL_SUM"
-
-cc <- left_join(cc, cc_group, by = "PRODUCT_TYPE")
-
-cc <- cc %>%
-  mutate(SUM_PCT = CUM_SUM / TTL_SUM) %>%
-  mutate(CC = if_else(SUM_PCT <= .6, "A",
-                      if_else(
-                        SUM_PCT <= .8, "B",
-                        if_else(SUM_PCT <= .95, "C",
-                                if_else(
-                                  SUM_PCT <= .99, "D",
-                                  if_else(SUM_PCT <= 1, "E", "E")
-                                ))
-                      )))
+insert_upload_job(
+  project = project,
+  dataset = "ecomm",
+  table = "cc_dim",
+  cc,
+  billing = project,
+  write_disposition = "WRITE_TRUNCATE"
+)
